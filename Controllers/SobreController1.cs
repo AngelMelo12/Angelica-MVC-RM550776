879939fbce34ac378cb1cc286ff36b7c0e7786db
AngelMelo12 <angellmello1993@gmail.com>
2024-03-21 11:49:38 -0300

    CP1 - MVC

---
+++ Controllers/SobreController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Angelica_MVC_RM550776.Controllers
+{
+    public class SobreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Angelica_MVC_RM550776.Models
+{
+    public class User : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
